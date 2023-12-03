81bf081cccd3994c554e6671c3fc2827becfbbc5
Shubham Badwial <shubhambadiwal8520@gmail.com>
2023-12-03 21:41:40 +0530

    landing page after auth

---
+++ src/app/homepage/page.js
@@ -0,0 +1,15 @@
+// function component
+"use client";
+import React from 'react';
+import Landingpage from '@/components/Landingpage';
+
+const homepage = () => {
+
+    return (
+      <>
+      <Landingpage />
+      </>
+    )
+  }
+  
+  export default homepage
